42b980be581ad16679e07d8c3817d76034518078
Theo M Jones <theomjones@gmail.com>
2018-05-20 17:29:44 +0100

    fixed path cap

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import classNames from './Post.module.css'
 import Header from '../components/Header/'
 import Container from '../components/Layout/Components/Container/index'
 
 import '../prism-themes/theme.css'
 
 const Post = ({ children }) => (
-  <div className={classNames.Post}>
+  <div>
     <Header />
     {children()}
   </div>
