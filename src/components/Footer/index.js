814bd62f82183eb2352e3115d3d09bc90512b3cc
Theo M Jones <theomjones@gmail.com>
2018-05-21 12:10:00 +0100

    spacing in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,9 +7,8 @@ const Footer = props => (
   <footer>
     <Hero withForm>
       <Text>
-        Built with React and <a href="https://www.gatsbyjs.org/">GatsbyJS</a>.<a href="https://github.com/theomjones/theomjones.com">
-          View Source.
-        </a>
+        Built with React and <a href="https://www.gatsbyjs.org/">GatsbyJS</a>.{' '}
+        <a href="https://github.com/theomjones/theomjones.com">View Source.</a>
       </Text>
       <Text>&copy; Theo Messenger-Jones - {new Date().getFullYear()}</Text>
     </Hero>
