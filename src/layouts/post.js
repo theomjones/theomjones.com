6638be9e444e8f7ebb4c1d8e98f70b6a13d11c73
Theo M Jones <theomjones@gmail.com>
2018-05-25 15:48:43 +0100

    prims css

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import Header from '../components/Header/'
 import Container from '../components/Layout/Components/Container/index'
 
-import '../prism-themes/theme.css'
+import 'prismjs/themes/prism-coy.css'
 
 const Post = ({ children }) => (
   <div>
